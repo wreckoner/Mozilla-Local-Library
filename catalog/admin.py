874d92d2bf3e43593d3876bf02cd12f1966b769f
Dibyendu Das <dibyendudas.1990@gmail.com>
2018-09-20 22:53:42 -0400

    First commit

---
+++ catalog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Book)
+admin.site.register(Language)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
